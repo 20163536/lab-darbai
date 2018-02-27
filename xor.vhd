059d48f67c355aacc9622a2296acaeae7eb25a43
20163536 <36884155+20163536@users.noreply.github.com>
2018-02-27 12:49:08 +0200

    Add files via upload

--- xor.vhd
+++ xor.vhd
@@ -1,14 +1,16 @@
+
 -- File: xor.vhd
+-- Compiled by: Evald Liachovic
 library ieee;
 use ieee.std_logic_1164.all;
-
+--evaldas
 -- the entity
 entity xor is
 port (
 i_a, i_b : in std_logic;
 o_f : out std_logic);
 end xor;
-
+--komentaras
 -- the architecture
 architecture xor_arc of xor is
 begin
